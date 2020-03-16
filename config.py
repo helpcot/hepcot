f969572e19f9ce280f235edec7fcb014b85ae3fb
helpcot <62214568+helpcot@users.noreply.github.com>
2020-03-16 21:31:04 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+TOKEN = 'Njg4NzAxMDAzMjg5MTk4NTk1.Xm4JAQ.gGb_qtHNVwSFbsebL82wpBLHr4Q'
+
+POST_ID = 688640786291621975 #post id to read reactions from
+
+# roles list according
+ROLES = {
+    '🐍': 687727672914083920, #Blue role
+    '🍇': 687727817680486415,
+    '💣': 687727876925161584,
+    '🔮': 687727943182844086,
+}
+
+# exclude this roles from counting
+EXCROLES = ()
+
+MAX_ROLES_PER_USER = 10 # max ammount of roles a can have
