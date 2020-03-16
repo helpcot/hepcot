d479a23c7a26ce81aa040438bfd0df52e3ddaa39
helpcot <62214568+helpcot@users.noreply.github.com>
2020-03-16 22:24:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,18 +39,3 @@ class MyClient(discord.Client):
 client = MyClient()
 token = os.environ.get('BOT_TOKEN')
 client.run(str(token))
-
-import os
-import discord
-from discord.ext import commands
-
-bot = commands.Bot(command_prefix='!')
-
-
-@bot.command(pass_context=True)  # разрешаем передавать агрументы
-async def test(ctx, arg):  # создаем асинхронную фунцию бота
-    await ctx.send(arg)  # отправляем обратно аргумент
-
-
-token = os.environ.get('BOT_TOKEN') # Получаем токен с heroku который ты указывал в настройках
-client.run(str(token)) # запускаем бота
