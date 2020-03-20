e10f75129c5dcb00d723bec8c84ab8bac627a374
helpcot <62214568+helpcot@users.noreply.github.com>
2020-03-20 10:08:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,17 +2,12 @@ import os
 import discord
 from discord.ext import commands
 import datetime
-import asyncio
-import random
 from discord.utils import get
 
 
 PREFIX = '/'
 
-colours = [discord.Color.dark_orange(),discord.Color.orange(),discord.Color.dark_gold(),discord.Color.gold(),discord.Color.dark_magenta(),discord.Color.magenta(),discord.Color.red(),discord.Color.dark_red(),discord.Color.blue(),discord.Color.dark_blue(),discord.Color.teal(),discord.Color.dark_teal(),discord.Color.green(),discord.Color.dark_green(),discord.Color.purple(),discord.Color.dark_purple()]
-delay = 0.2
-serverid = 687635128285003817
-rainbowrolename = "Элита"
+
 client = commands.Bot( command_prefix = PREFIX )
 client.remove_command( 'help' )
 
@@ -169,40 +164,6 @@ async def sex( ctx, member: discord.Member ):
 
 	await member.add_roles( sex_role )
 	await ctx.send( f'У { member.mention }, появилась топ роль!' )
-
-async def rainbowrole(role):
-    for role in client.get_guild(serverid).roles:
-        if str(role) == str(rainbowrolename):
-            print("detected role")
-            while not client.is_closed():
-                try:
-                    await role.edit(color=random.choice(colours))
-                except Exception:
-                    print("can't edit role, make sure the bot role is above the rainbow role and that is have the perms to edit roles")
-                    pass
-                await asyncio.sleep(delay)
-    print('role with the name "' + rainbowrolename +'" not found')
-    print("creating the role...")
-    try:
-        await client.get_guild(serverid).create_role(reason="Created rainbow role", name=rainbowrolename)
-        print("role created!")
-        await asyncio.sleep(2)
-        client.loop.create_task(rainbowrole(rainbowrolename))
-    except Exception as e:
-        print("couldn't create the role. Make sure the bot have the perms to edit roles")
-        print(e)
-        pass
-        await asyncio.sleep(10)
-        client.loop.create_task(rainbowrole(rainbowrolename))
-
-@client.event
-async def on_ready():
-    client.loop.create_task(rainbowrole(rainbowrolename))
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('Ready.')
-    print('------------')
 
 @client.command( pass_context = True)
 @commands.has_permissions( administrator = True )	
