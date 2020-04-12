4600b3d9ed3b0c9996dedf00fb7ecd018cc42de7
helpcot <62214568+helpcot@users.noreply.github.com>
2020-04-12 11:22:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -108,7 +108,7 @@ async def members(ctx):
 @commands.has_permissions( administrator = True )
 async def tempmute(ctx,amount : int,member: discord.Member = None, reason = None):
     mute_role = discord.utils.get(member.guild.roles, id = 698619622852788335) #Айди роли
-    channel_log = bot.get_channel(698603287032758404) #Айди канала логов
+    channel_log = bot.get_channel(698603287032758407) #Айди канала логов
 
     await member.add_roles( mute_role )
     await ctx.send(embed = discord.Embed(description = f'**:shield: Пользователю {member.mention} был ограничен доступ к чатам.\n:book: По причине: {reason}**', color=0x0c0c0c)) 
