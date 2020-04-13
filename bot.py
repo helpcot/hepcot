4cd202d27e73eea4d9a9feb337f60946d3540eed
helpcot <62214568+helpcot@users.noreply.github.com>
2020-04-13 10:38:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -280,6 +280,17 @@ async def mute(ctx, amount : int, member: discord.Member = None, role: discord.R
 	await member.remove_roles( mute_role )
 
 
+@client.event
+
+async def on_member_join(member ):
+    channel = client.get_channel( 698940462488092742 )
+
+    role = discord.utils.get( member.guild.roles, id = 699156735474008064 )
+
+    await member.add_roles( role )
+    await channel.send( embed = discord.Embed( description = f'Пользователь ``{ member.name }``, присоеденился к нам!', color = 0x0c0c0c) )	
+	
+	
 token = os.environ.get('BOT_TOKEN') # Получаем токен с heroku который ты указывал в настройках
 client.run(str(token)) # запускаем бота
 
