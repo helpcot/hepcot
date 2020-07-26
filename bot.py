509bab7c501c264b8043e98081c698cbb493378a
helpcot <62214568+helpcot@users.noreply.github.com>
2020-07-26 20:26:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -831,52 +831,6 @@ async def prefix(ctx, prfx=None):
             await ctx.send("Пожалуйста введите префикс")
 	
 
-default_role="Собака Сутулая"      
-@client.event
-async def on_message(message):
-        global dothething
-        if message.author == client.user:
-                return
-        if message.content.startswith("+stop"):
-                await client.send_message(message.channel,"stopped")
-                try:
-                        dothething[str(message.server.id)]=0
-                except:
-                        print("err")
-        if message.content.startswith("+start"):
-                await client.send_message(message.channel, "started")
-                hue=0
-                if message.content.strip().startswith("+start "):
-                        role = discord.utils.find(lambda m: m.name == message.content[6:].strip() ,message.server.roles)
-                else:
-                        role = discord.utils.find(lambda m: m.name == default_role ,message.server.roles)
-                try:
-                        dothething[str(message.server.id)]
-                except:
-                        dothething[str(message.server.id)]=0
-                if role and not dothething[str(message.server.id)]:
-                        dothething[str(message.server.id)]=1
-                        while dothething[str(message.server.id)]:
-                                users = [int(str(x.status)=="online") for x in message.server.members if role in x.roles] #black magic fuckery here
-                                users.append(0)
-                                print(str(message.server.name)+" - "+str(users))
-                                if max(users):
-                                        for i in range(50): #arbitrary rate limits
-                                                if not dothething[str(message.server.id)]:
-                                                        break
-                                                hue = (hue+7)%360
-                                                rgb = [int(x*255) for x in hls_to_rgb(hue/360, 0.5, 1)]
-                                                await asyncio.sleep(1/5)
-                                                clr = discord.Colour(((rgb[0]<<16) + (rgb[1]<<8) + rgb[2]))
-                                                try:
-                                                        await client.edit_role(message.server, role, colour=clr)
-                                                except:
-                                                        print("no perms" + str(message.server.name))
-                                                        dothething[str(message.server.id)]=0
-                                else:
-                                        await asyncio.sleep(10)
-
-
 
 token = os.environ.get('BOT_TOKEN') # Получаем токен с heroku который ты указывал в настройках
 client.run(str(token)) # запускаем бота
