56790a0d4cf736919035c06b649aef58fd56e727
helpcot <62214568+helpcot@users.noreply.github.com>
2020-07-26 20:16:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -842,27 +842,15 @@ colours = [discord.Color.dark_orange(),discord.Color.orange(),discord.Color.dark
 async def rainbowrole(role):
     for role in client.get_guild(serverid).roles:
         if str(role) == str(rainbowrolename):
-            print("detected role")
             while not client.is_closed():
                 try:
                     await role.edit(color=random.choice(colours))
                 except Exception:
-                    print("can't edit role, make sure the bot role is above the rainbow role and that is have the perms to edit roles")
                     pass
                 await asyncio.sleep(delay)
-    print('role with the name "' + rainbowrolename +'" not found')
-    print("creating the role...")
+
     try:
         await client.get_guild(serverid).create_role(reason="Created rainbow role", name=rainbowrolename)
-        print("role created!")
-        await asyncio.sleep(2)
-        client.loop.create_task(rainbowrole(rainbowrolename))
-    except Exception as e:
-        print("couldn't create the role. Make sure the bot have the perms to edit roles")
-        print(e)
-        pass
-        await asyncio.sleep(10)
-        client.loop.create_task(rainbowrole(rainbowrolename))
 
 
 
